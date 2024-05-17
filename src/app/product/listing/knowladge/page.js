7dccfbe9bc45020b0d53e529605501122a03136f
TTruonghuy <ttruonghuy2003@gmail.com>
2024-05-17 12:23:44 +0700

    delete folder

--- src/app/product/listing/knowladge/page.js
+++
@@ -1,8 +0,0 @@
-import CommonListing from "@/components/CommonListing";
-import { productByCategory } from "@/services/product";
-
-export default async function knowladgeAllProducts() {
-  const getAllProducts = await productByCategory("knowladge");
-
-  return <CommonListing data={getAllProducts && getAllProducts.data} />;
-}
--- src/app/product/listing/literary/page.js
+++
@@ -1,8 +0,0 @@
-import CommonListing from "@/components/CommonListing";
-import { productByCategory } from "@/services/product";
-
-export default async function literaryAllProducts() {
-  const getAllProducts = await productByCategory("literary");
-
-  return <CommonListing data={getAllProducts && getAllProducts.data} />;
-}
